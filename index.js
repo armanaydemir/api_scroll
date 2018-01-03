4b949df84e5d559f127dabf5566eb540554ccfe9
armanaydemir <arman.aydemir@live.com>
2018-01-03 01:16:32 -0500

    last commit??

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var bodyParser = require("body-parser");
 const cheerio = require('cheerio')
 var app = express();
 var request = require('request');
+var fs = require('fs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,39 +14,33 @@ var headers = {
 
 var sections = []
 
-// nytimes articles for testing -----
-// https://www.nytimes.com/2017/02/01/magazine/the-misunderstood-genius-of-russell-westbrook.html
-// https://www.nytimes.com/2017/11/22/us/politics/alliance-defending-freedom-gay-rights.html
-// https://www.nytimes.com/2017/11/21/technology/bitcoin-bitfinex-tether.html
-
 function parse_body(body) {
 	const $ = cheerio.load(body);
 	const bodies = $('p');
 	var i = 0;
-	sections = []; // need to fix this so it gets everything and not just the first tag of text
+	sections = []; 
 	while(i < bodies.length){
 		var o = 0;
 		var subsections = [];
 		//console.log(bodies[i].children.length)
 		while(o < bodies[i].children.length){
 			if(bodies[i].children[o].type == 'text'){
-				subsections.push(bodies[i].children[o].data);
-				console.log(bodies[i].children[o].data)
+				subsections.push(bodies[i].children[o].data.replace('\\n',''));
+				//console.log(bodies[i].children[o].data)
 			}
 			else if(bodies[i].children[o].type == 'tag'){
-				console.log(bodies[i].children[o].children[0].data)
-				subsections.push(bodies[i].children[o].children[0].data);
+				subsections.push(bodies[i].children[o].children[0].data.replace('\\n',''));
+				//console.log(bodies[i].children[o].children[0].data)
 			}
 			o ++;
-			//console.log(subsections);
 		}	
 		//console.log(subsections);
 		//console.log(subsections.join(''));
 		//console.log('------------------------')
-		sections.push(subsections.join());
+		sections.push((i+1) + subsections.join('')); // i + 1 is for debugging
 		i ++;
 	}
-	console.log(sections);
+	console.log(sections[sections.length-1]);
 }
 
 function init_article(address) {
@@ -60,16 +55,27 @@ function init_article(address) {
 	});
 }
 
-init_article("https://www.nytimes.com/2017/11/21/technology/bitcoin-bitfinex-tether.html");
+// nytimes articles for testing -----
+// https://www.nytimes.com/2017/02/01/magazine/the-misunderstood-genius-of-russell-westbrook.html
+// https://www.nytimes.com/2017/11/22/us/politics/alliance-defending-freedom-gay-rights.html
+// https://www.nytimes.com/2017/11/21/technology/bitcoin-bitfinex-tether.html
+
+article = "https://www.nytimes.com/2017/11/22/us/politics/alliance-defending-freedom-gay-rights.html"
+
+init_article(article);
+
+article = article.split('/')
+article = article[article.length -1]
 
 app.get("/", function(req, res) {
     res.send(sections);
 });
 
 app.post("/submit_data", function(req, res) {
-	console.log('bitch i might might')
-	console.log(req);
-	console.log(res);
+	var data = req.body
+	fs.appendFileSync(data.device_id + ':' + data.startTime + ':' + article + '.csv', JSON.stringify(data) + '\n')
+	//writer.write(data)
+	res.sendStatus(200)
 });
 
 var server = app.listen(3000, function () {
