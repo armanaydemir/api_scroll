a4a14d303ff72ce05bf0f56d6f89b89d19fc200f
Arman Aydemir <armana22@gmail.com>
2018-08-17 17:38:54 -0400

    purge function added almost

--- db.js
+++ db.js
@@ -1,31 +1,38 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
+
+//need to finish this ... export to csv??
 function export_data() {
 	MongoClient.connect(url, function(e, db){
-		if(e) throw e;
 		var dbd = db.db('data')
 		var dbsessions = db.db('sessions')
+		if(e) throw e;
 		dbd.collection('sessions').find().toArray(function(err, sessions){
 			if(err) throw err;
 			console.log(sessions)
 			db.close()
 		});
-	})
+	});
 }
 
 
 
-// function purge_data() {
-// 	MongoClient.connect(url, function(e, db){
-// 		if(e) throw e;
-// 	})
-// }
+function purge_data() {
+	MongoClient.connect(url, function(e, db){
+		var dbsessions = db.db('sessions')
+		var dbd = db.db('data')
+		if(e) throw e;
+		dbd.collection('sessions').find().toArray(function(error, c){
+			if(error) throw error;
+			var complete = c.map(x => x.session_db_link)
+			dbsessions.listCollections().toArray(function(err, s){
+				if(err) throw err;
+				console.log(s.filter(id => complete.includes(id)))
+			});
+		});
+	});
+}
 
 
-// function delete_data() {
-
-// }
-
-export_data()
-
+function purge_data()
