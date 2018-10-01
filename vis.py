b45d1992a9c1417125c35c23f1d4543e383c2c47
Arman Aydemir <armana22@gmail.com>
2018-10-01 16:59:45 -0600

    printing collection

--- vis.py
+++ vis.py
@@ -1,3 +1,11 @@
 import pymongo
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["data"]
+
+def printcol(c):
+	mycol = mydb[c]
+	for x in mycol.find():
+		print(x)
+
+printcol("2D61165D_CDA0_42BF_9A88_F2E2C384F33455995508133834600")
