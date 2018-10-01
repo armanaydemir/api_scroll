fe311b56888637bbae42054774e19b81bd1d468f
Arman Aydemir <armana22@gmail.com>
2018-10-01 17:09:42 -0600

    first completed session

--- vis.py
+++ vis.py
@@ -1,11 +1,21 @@
 import pymongo
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["sessions"]
+sessions = myclient["sessions"]
+data = myclient["data"]
+
+def firstcompletedsession():
+	mycol = data["sessions"]
+	for x in mycol.find()
+		if(x.completed):
+			print x
+
 
 def printcol(c):
-	mycol = mydb[c]
+	mycol = sessions[c]
 	for x in mycol.find():
 		print(x)
 
-printcol("2D61165D_CDA0_42BF_9A88_F2E2C384F33455995508133834600")
+
+firstcompletedsession()
+#printcol("2D61165D_CDA0_42BF_9A88_F2E2C384F33455995508133834600")
