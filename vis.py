da3849da0a7f334ad2bb3c73925cf146c3150be6
Arman Aydemir <armana22@gmail.com>
2018-10-01 17:16:06 -0600

    dubm syntax

--- vis.py
+++ vis.py
@@ -8,7 +8,7 @@ def firstcompletedsession():
 	mycol = data["sessions"]
 	for x in mycol.find():
 		if(x["completed"]):
-			print x
+			print x['UDID'] + x['startTime']
 
 
 def printcol(c):
@@ -19,3 +19,7 @@ def printcol(c):
 
 firstcompletedsession()
 #printcol("2D61165D_CDA0_42BF_9A88_F2E2C384F33455995508133834600")
+
+
+#2D61165D_CDA0_42BF_9A88_F2E2C384F334
+#5.59860231422299e+16
