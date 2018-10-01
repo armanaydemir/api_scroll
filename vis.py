ecf3637b4e1284ca006c1aba4c35644d8ca144b4
Arman Aydemir <armana22@gmail.com>
2018-10-01 17:32:52 -0600

    average time spent on screen per line

--- vis.py
+++ vis.py
@@ -1,17 +1,9 @@
 import pymongo
 import decimal
 
-# create a new context for this task
-ctx = decimal.Context()
-
-# 20 digits should be enough for everyone :D
-ctx.prec = 20
-
-def float_to_str(f):
-    """
-    Convert the given float to a string,
-    without resorting to scientific notation
-    """
+def float_to_str(f): #https://stackoverflow.com/questions/38847690/convert-float-to-string-without-scientific-notation-and-false-precision
+	ctx = decimal.Context()
+	ctx.prec = 20
     d1 = ctx.create_decimal(repr(f))
     return format(d1, 'f')
 
@@ -19,11 +11,13 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 sessions = myclient["sessions"]
 data = myclient["data"]
 
-def firstcompletedsession():
+def findcompletedsessions():
 	mycol = data["sessions"]
+	completed = []
 	for x in mycol.find():
 		if(x["completed"]):
-			printcol(x['UDID'] + float_to_str(x['startTime']))
+			completed.append(x['UDID'] + float_to_str(x['startTime']))
+	return completed
 
 
 def printcol(c):
@@ -31,8 +25,20 @@ def printcol(c):
 	for x in mycol.find():
 		print(x)
 
+def averageperline(c):
+	times = [None]*1000
+	mycol = sessions[c]
+	for row in mycol.find():
+		for i in range(int(row["first_line"]), int(row["last_line"]))
+			times[i] += row["time"] - row["appeared"]
+	return times
 
-firstcompletedsession()
+
+sessions = findcompletedsessions()
+print averageperline(sessions[0])
+
+
+
 #printcol("2D61165D_CDA0_42BF_9A88_F2E2C384F33455995508133834600")
 
 
