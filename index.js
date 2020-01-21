2fda9664f5ec52796cd6f9f15cc38a6dd3e90964
Arman Aydemir <arman.aydemir@live.com>
2020-01-20 17:06:03 -0700

    adding article data

--- index.js
+++ index.js
@@ -243,7 +243,7 @@ app.post('/sessions', function(req,res){
 		var dbd = db.db(database) //'UDID': data.UDID, 
 		console.log('woah')
 		dbd.collection(combined_sessions_collection).find({'completed':true}).sort({datefield: 1}).map(function(result){
-			doc = dbd.collection(combined_articles_collection).findOne({'_id': ObjectId(result.article_id)})
+			doc = await dbd.collection(combined_articles_collection).findOne({'_id': ObjectId(result.article_id)})
 			if(doc){
 				console.log(doc)
 				doc.result = result
