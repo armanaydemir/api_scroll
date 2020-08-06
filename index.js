57881d0e8bbfc413bcd7905e93656d8c7c8dc8a9
Arman Aydemir <arman.aydemir@live.com>
2020-08-06 13:26:17 -0600

    woah

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var url = "mongodb://localhost:27017/";
 
 var sessionsCollection = 'complete_sessions01'
 var articlesCollection = 'complete_articles01'
-var database = 'data037temp60'
+var database = 'data037temp61'
 
 var old_db = 'data'
 var old_sessions = 'sessions'
@@ -26,8 +26,8 @@ var combined_sessions_collection = 'complete_sessions01'
 var combined_articles_collection = 'complete_articles01'
 
 
-var sessions_db = 'sessions09'
-var events_db = 'events09'
+var sessions_db = 'sessions10'
+var events_db = 'events10'
 
 
 
