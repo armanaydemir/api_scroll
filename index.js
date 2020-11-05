d034e6d9d37a01f321e50166b8cd999f44f2c9bb
Arman Aydemir <arman.aydemir@live.com>
2020-11-05 13:24:58 -0800

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ var articlesCollection = 'complete_articles01'
 var emailsCollection = "complete_emails01"
 
 //very important
-var database = 'data037temp80'
-var sessions_db = 'sessions80'
-var events_db = 'events80'
-var questions_db = 'questions80'
+var database = 'data037temp81'
+var sessions_db = 'sessions81'
+var events_db = 'events81'
+var questions_db = 'questions81'
 
 var old_db = 'data'
 var old_sessions = 'sessions'
