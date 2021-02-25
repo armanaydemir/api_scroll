41bc6301e6c89b548846edc8d97c6631a2975195
armanaydemir <arman.aydemir@live.com>
2021-02-25 12:00:19 -0700

    removing read articles

--- index.js
+++ index.js
@@ -486,7 +486,7 @@ app.get('/articles', function(req, res){
 		var dbd = db.db(database)
 		dbd.collection(combined_articles_collection).find({}).sort({_id: -1}).toArray(function(er, results) {
 			if(er) throw er;
-			results.filter(result => articles_filter_helper(dbd,result, data).then(new_data => {
+			results.filter(result => articles_filter_helper(dbd,result, data)).then(new_data => {
 			// articles_helper(dbd,results, data).then(new_data => {
 				console.log(new_data.length)
 				res.send(new_data)
@@ -514,9 +514,9 @@ async function articles_filter_helper(dbd,result, data){
 	}
 }
 
-async function articles_helper(dbd, results, data){
-	return Promise.all())
-}
+// async function articles_helper(dbd, results, data){
+// 	return Promise.all())
+// }
 
 // app.get('/nyt_scrape_one', function(req, res){
 // 	var data = req.body
