241d8ceddba8454587fdb25f7d309e607d3c51e3
armanaydemir <arman.aydemir@live.com>
2021-04-24 21:12:57 -0600

    data

--- model.py
+++ model.py
@@ -289,8 +289,9 @@ for i in range(0,len(users)):
 	lst = data
 	split_data = [lst[i:i + n] for i in range(0, len(lst), n)]
 	print(len(split_data))
-	folds = [(split_data[i], split_data[:i].append(split_data[i:])) for i in range(1, len(split_data)) ]
-	folds = [(split_data[0], split_data[0:])].append(folds)
+	tfolds = [(split_data[i], split_data[:i].append(split_data[i:])) for i in range(1, len(split_data)) ]
+	folds = [(split_data[0], split_data[0:])]
+	folds.append(tfolds)
 	print(len(folds))
 	print(len(folds[0][0]))
 	print(len(folds[0][1]))
